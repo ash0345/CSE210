93e2020ddb54c80ddcd23d31fa49fc280158adf5
ash0345 <ang21002@byui.edu>
2023-01-06 13:56:14 -1000

    Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeString = Console.ReadLine();
+        int grade = int.Parse(gradeString);
+
+        char letterGrade;
+
+        if (grade >= 90)
+        {
+            letterGrade = 'A';
+        }
+        else if (grade >= 80)
+        {
+            letterGrade = 'B';
+        }
+        else if (grade >= 70)
+        {
+            letterGrade = 'C';
+        }
+        else if (grade >= 60)
+        {
+            letterGrade = 'D';
+        }
+        else
+        {
+            letterGrade = 'F';
+        }
+
+        Console.WriteLine($"Your letter grade is {letterGrade}");
+
+        if (letterGrade == 'A' || letterGrade == 'B' || letterGrade == 'C')
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
