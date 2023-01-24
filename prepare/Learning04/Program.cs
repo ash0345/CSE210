0eff1d645f9d8d34059fe10072687d833ed239ba
ash0345 <ang21002@byui.edu>
2023-01-23 21:26:40 -1000

    Learning 04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment first = new Assignment("Ashley Anglesey", "Code");
+        
+        Console.WriteLine(first.GetSummary()); 
+        Console.WriteLine();
+
+        MathAssignment math = new MathAssignment("Ashley Anglesey", "Multiplication", "7.3", "1-20");
+
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment english = new WritingAssignment("Ashley Anglesey", "Literature", "Huck Fin");
+        
+        Console.WriteLine(english.GetSummary());
+        Console.WriteLine(english.GetWritingInfo());
     }
 }
