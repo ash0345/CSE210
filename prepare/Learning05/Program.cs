7531ed2fc69dc1a0a5b66de959948498d6b5dae4
ash0345 <ang21002@byui.edu>
2023-01-31 17:26:18 -1000

    Learning05

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+
+        Square square = new Square("Blue", 4);
+        Rectangle rectangle = new Rectangle("Red", 3, 5);
+        Circle circle = new Circle("Pink", 5);
+
+        List<Shape> shapes = new List<Shape>();
+        shapes.Add(square);
+        shapes.Add(rectangle);
+        shapes.Add(circle);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"Color:{color}\nArea:{area}\n");
+        }
     }
 }
